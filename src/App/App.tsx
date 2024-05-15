973365d89d8614895f6f38b9f7a3693acfbdafac
BatBeka <bekarichman@gmail.com>
2024-05-15 14:52:59 +0400

    Add some styles, add part of a header

---
+++ src/App/App.tsx
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
+import * as Container from "../Containers";
+
+const App = () => {
+  return (
+    <div className="px-xl-5 px-md-4 px-sm-2 px-1 py-3">
+      <header>
+        <Container.Header />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+ 
+export default App;
---
+++ src/Containers/Header/Header.tsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="text-center text-lg-start">
+      <h1 className="tracking-in-expand title display-5">Beka Aladashvili</h1>
+      <p className="tracking-in-expand title-underline fst-italic fs-5">Your local web developer</p>
+    </div>
+  );
+}
+ 
+export default Header;
